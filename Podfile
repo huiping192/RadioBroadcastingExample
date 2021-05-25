cdb7912c3e35c9c0d1e12ed6448391b45aa843ec
huiping_guo <huiping.guo@dena.jp>
2021-05-25 23:29:52 +0900

    project init

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '11.0'
+swift_version = '5.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'RadioBroadcastingExample'  do
+    platform :ios, '9.0'
+    pod 'HaishinKit'
+end
+
---
+++ RadioBroadcastingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RadioBroadcastingExample
+//
+//  Created by Huiping Guo on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
